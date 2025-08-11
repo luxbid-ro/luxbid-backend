1a54b9d1c2bfbe2607cfe27a33b5535b32134b6a
Andrei Ionut Tufa <andrei.ionut91@icloud.com>
2025-08-11 10:55:05 +0300

    Prisma: add error logging and graceful shutdown; add DB diagnostics

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,26 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      log: [
+        { emit: 'event', level: 'error' },
+      ],
+    });
+
+    this.$on('error', (e) => {
+      // eslint-disable-next-line no-console
+      console.error('Prisma error:', e);
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
