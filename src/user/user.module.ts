6aa11da4d984b7d1182cb385f56fa1b32d5eaacd
Andrei Ionut Tufa <andrei.ionut91@icloud.com>
2025-08-13 23:04:55 +0300

    🔧 FIX: UserModule imports PrismaModule for DI

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { CloudinaryDbService } from '../cloudinary-db.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UserController],
   providers: [UserService, CloudinaryDbService],
   exports: [UserService],
