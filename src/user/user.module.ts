81a8fd382960cf05efa6664c109ad39a9ba108fd
Andrei Ionut Tufa <andrei.ionut91@icloud.com>
2025-08-13 23:07:15 +0300

    🔧 EMERGENCY FIX: Revert to working UserController without UserService

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
-import { UserService } from './user.service';
-import { CloudinaryDbService } from '../cloudinary-db.service';
-import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [UserController],
-  providers: [UserService, CloudinaryDbService],
-  exports: [UserService],
 })
 export class UserModule {}
