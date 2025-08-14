9406f6b46c8eb04937d97a9cb92244a5ff080a40
Andrei Ionut Tufa <andrei.ionut91@icloud.com>
2025-08-14 08:26:47 +0300

    Add missing user API endpoints for profile management

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Put, Post, Delete, Body, UseGuards, Request } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt.guard';
+import { UserService } from './user.service';
+import { UpdateProfileDto, ChangePasswordDto } from './dto/update-profile.dto';
 
 @Controller('users')
 @UseGuards(JwtAuthGuard)
 export class UserController {
+  constructor(private userService: UserService) {}
+
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
@@ -13,5 +17,20 @@ export class UserController {
   @Get('me')
   getMe(@Request() req) {
     return req.user;
+  }
+
+  @Put('profile')
+  async updateProfile(@Request() req, @Body() updateProfileDto: UpdateProfileDto) {
+    return this.userService.updateProfile(req.user.id, updateProfileDto);
+  }
+
+  @Post('change-password')
+  async changePassword(@Request() req, @Body() changePasswordDto: ChangePasswordDto) {
+    return this.userService.changePassword(req.user.id, changePasswordDto);
+  }
+
+  @Delete('account')
+  async deleteAccount(@Request() req) {
+    return this.userService.deleteAccount(req.user.id);
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { CloudinaryDbService } from '../cloudinary-db.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UserController],
+  providers: [UserService, CloudinaryDbService],
+  exports: [UserService],
 })
 export class UserModule {}
